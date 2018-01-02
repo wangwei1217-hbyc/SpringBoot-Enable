4a24a208f42059500d77e8584791df4edbbe8020
wangwei <wangwei_hbyc@aliyun.com>
2018-01-02 23:01:13 +0800

    Girl.java Add field

--- src/main/java/com/wamgwei/enable/Girl.java
+++ src/main/java/com/wamgwei/enable/Girl.java
@@ -4,4 +4,22 @@ package com.wamgwei.enable;
  * Created by wangwei on 2018/1/2.
  */
 public class Girl {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
